e9580af21c8119d262ae5f4d784976c336d74eec
Clarisse Moraes <clarissem95@live.com>
2020-01-11 00:53:10 -0500

    got notes to console log as an object--still need to make saved note appear on page

--- server.js
+++ server.js
@@ -2,15 +2,18 @@
 const express = require ("express");
 const path = require ("path");
 const fs = require ("fs");
+const bodyParser = require('body-parser');  
 
 const app = express();
+//Connect to a PORT
 const PORT = process.env.PORT || 3000;
 
 //Data Parsing
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static("public"));
-  
+app.use(bodyParser.json());
+
 
 //HTML Routes
 app.get("/notes", function (req, res){
@@ -23,45 +26,33 @@ app.get("/", function (req, res){
 });
 
 // API Routes
-//  ----ATTEMPT 1------
-// app.get("/api/notes", function(req, res){
-//     fs.readFile("/db.json", utf8, function(err,data){
-//         if (err) throw err;
-//     })
-// })
-//-----ATTEMPT 2------
-// app.get("/api/notes", function(req, res) {
-//     res.sendFile(path.join(__dirname, "/db/db.json"));
-// });
-// -----ATTEMPT 3-----
-// app.get("/api/notes", function (req, res) {
-//     fs.readFile( __dirname + "/" + "db.json", 'utf8', function (err, data) {
-//        console.log( data );
-//        res.end( data );
-//     });
-//  })
-
-
-
-
-app.post("/api/notes", (res)=> {
-    console.log("Note Posted!");
-    var newNote = request.body;
-    noteInfo.push(newNote);
-    const parsedData = JSON.stringify(noteInfo);
-    fs.writeFile(path.join("db.json"), (err) =>{
+app.get("/api/notes", function(req, res){
+    fs.readFile("./db/db.json", function(err,res){
         if (err) throw err;
-        res.json(noteInfo);
-})
-})
+    }); 
+});
 
-app.delete("/api/notes/:id", ()=> {
-    //how they grab of the id from the front end-- 
-    fs.readFile
-    fs.writeFile
-    //when the data comes in your need to add a property and value to id that is not repeated//
+
+app.post("/api/notes", function(req, res) {
+    var note= req.body
+    console.log(note);
+   
+});
+
+
+
+
+
+
+
+
+// app.delete("/api/notes/:id", ()=> {
+//     //how they grab of the id from the front end-- 
+//     fs.readFile
+//     fs.writeFile
+//     //when the data comes in your need to add a property and value to id that is not repeated//
     
-})
+// })
 
 
 
